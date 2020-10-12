22af8b052c47c26889b7ee163bbbcdf817bbb3c0
Xubili <guivpw68@gmail.com>
2020-10-12 04:51:28 -0300

    added email sender

---
+++ server/src/utils/email/email.ts
@@ -0,0 +1,235 @@
+interface User {
+  id: number
+  name: string
+}
+export const Mail = ({ id, name }: User): string => {
+  return `<table width="100%" height="100%" align="center" border="0" cellpadding="0" cellspacing="0"
+	style="background-color:#1a1e2c;border:0;border-collapse:collapse;border-spacing:0;vertical-align: center;"
+	bgcolor="#1a1e2c">
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
+	</style>
+	<tbody>
+		<tr>
+			<td align="center" style="display:block">
+
+				<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0"
+					style="border:0;border-collapse:collapse;border-spacing:0;max-width:700px">
+					<tbody>
+						<tr>
+							<td style="background-color:#0A0E13">
+
+								<table width="100%" border="0" cellpadding="0" cellspacing="0"
+									style="border:none;border-collapse:collapse;border-spacing:0;width:100%">
+									<tbody>
+										<tr>
+											<td align="center"
+												style="direction:ltr;text-align:center;padding:10px 14px 10px 14px;padding-center:0;background-color:#0A0E13"
+												bgcolor="#0A0E13">
+
+												<table border="0" cellpadding="0" cellspacing="0"
+													style="border:none;border-collapse:collapse;border-spacing:0;width:100%">
+													<tbody>
+														<tr>
+															<td style="direction:ltr;text-align:center;font-size:0">
+
+																<table border="0" cellpadding="0" cellspacing="0" class="m_-5732199361944882318t11of12"
+																	style="border:none;border-collapse:collapse;border-spacing:0;max-width:616px;width:100%;display:inline-block;vertical-align:middle">
+																	<tbody>
+																		<tr>
+																			<td style="direction:ltr;text-align:center;padding-center:0;padding-right:0">
+																				<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center"
+																					style="border:none;border-collapse:collapse;border-spacing:0;table-layout:fixed;width:100%">
+																					<tbody>
+																						<tr>
+																							<td style="direction:ltr;text-align:left;font-size:0">
+
+																								<table border="0" cellpadding="0" cellspacing="0"
+																									class="m_-5732199361944882318t4of12"
+																									style="border:none;border-collapse:collapse;border-spacing:0;display:inline-block;max-width:408px;vertical-align:middle;width:100%">
+																									<tbody>
+																										<tr>
+																											<td
+																												style="direction:ltr;text-align:center;padding-center:12px;padding-right:12px">
+																												<table border="0" cellpadding="0" cellspacing="0" width="100%"
+																													align="left"
+																													style="border:none;border-collapse:collapse;border-spacing:0;table-layout:fixed;width:100%">
+																													<tbody>
+																														<tr>
+																															<td style="direction:ltr;text-align:left;font-size:0">
+
+																																<table border="0" cellpadding="0" cellspacing="0"
+																																	width="100%" align="left"
+																																	style="border:none;border-collapse:collapse;border-spacing:0;table-layout:fixed;width:100%">
+																																	<tbody>
+																																		<tr>
+																																			<td
+																																				style="direction:ltr;text-align:left;color:#000000;font-family:'UberMoveText-Regular','HelveticaNeue',Helvetica,Arial,sans-serif;font-size:0;line-height:26px;padding-bottom:0px;padding-top:0px">
+
+																																				<img src="http://i.imgur.com/GKDfNy1.png"
+																																					width="110" height="96" alt="Cup Center"
+																																					style="clear:both;display:block;max-width:100%;outline:none;text-decoration:none;margin-top: 50px;"
+																																					class="CToWUd">
+
+																																			</td>
+																																		</tr>
+																																	</tbody>
+																																</table>
+																															</td>
+																														</tr>
+
+																													</tbody>
+
+																												</table>
+																											</td>
+																										</tr>
+																									</tbody>
+																								</table>
+																							</td>
+																						</tr>
+																					</tbody>
+																				</table>
+																			</td>
+																		</tr>
+																	</tbody>
+																</table>
+
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+
+									</tbody>
+								</table>
+
+								<table width="100%" border="0" cellpadding="0" cellspacing="0"
+									style="border:none;border-collapse:collapse;border-spacing:0;width:100%">
+									<tbody>
+										<tr>
+											<td align="center" style="direction:ltr;text-align:center;padding:0 14px 0 14px">
+												<table border="0" cellpadding="0" cellspacing="0"
+													style="border:none;border-collapse:collapse;border-spacing:0;width:100%">
+													<tbody>
+														<tr>
+															<td style="direction:ltr;text-align:center">
+
+																<table border="0" cellpadding="0" cellspacing="0" class="m_-5732199361944882318t1of12"
+																	align="center"
+																	style="border:none;border-collapse:collapse;border-spacing:0;max-width:56px;width:100%">
+																	<tbody>
+																		<tr>
+																			<td style="direction:ltr;text-align:center;padding-center:12px;padding-right:12px">
+																				<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center"
+																					style="border:none;border-collapse:collapse;border-spacing:0;table-layout:fixed;width:100%">
+																					<tbody>
+																						<tr>
+																							<td height="2"
+																								style="direction:ltr;text-align:center;font-size:0;line-height:1px">&nbsp;
+
+																							</td>
+																						</tr>
+																					</tbody>
+																				</table>
+																			</td>
+																		</tr>
+																	</tbody>
+																</table>
+
+																<table border="0" cellpadding="0" cellspacing="0" class="m_-5732199361944882318t11of12"
+																	align="center"
+																	style="border:none;border-collapse:collapse;border-spacing:0;max-width:616px;width:100%">
+																	<tbody>
+																		<tr>
+																			<td style="direction:ltr;text-align:center;padding-center:0;padding-right:0">
+																				<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center"
+																					style="border:none;border-collapse:collapse;border-spacing:0;table-layout:fixed;width:100%">
+																					<tbody>
+																						<tr>
+																							<td style="direction:ltr;text-align:left">
+																								<p
+																									style="color: #cecece;line-height: 200%;font-size: 17pt;font-family: 'Nunito Sans', 'Nunito', sans-serif;">
+																									Olá ${name}, seja bem-vindo(a) a plataforma Cup Center!<br />Antes
+																									de prosseguir, confirme o seu cadastro realizando a <span
+																										style="color: #709dff; font-family: 'Nunito Sans',  'Nunito', sans-serif;">verificação
+																										de email</span>,
+																									clicando no botão abaixo:</p>
+																								<p>
+																									<table align="center">
+																										<tr>
+																											<td></td>
+																										</tr>
+																										<tr>
+																											<td></td>
+																										</tr>
+																										<tr>
+																											<td></td>
+																										</tr>
+																										<tr>
+																											<td>
+																												<a style="margin-top: 2em; background-color: #709dff; border-bottom:6px solid #4861b6; padding: 20px; border-radius: 5px; outline: 0; text-decoration: none; color: #FFF; font-weight: 500; font-size: 17pt; font-family: 'Nunito', 'Nunito Sans', sans-serif;	 text-align: center; transition: background-color .5s;"
+																													href="http://localhost:${process.env.PORT}/verifyEmail/${id}">Verifique seu email</a>
+																											</td>
+																										</tr>
+																										<tr>
+																											<td></td>
+																										</tr>
+																										<tr>
+																											<td></td>
+																										</tr>
+																										<tr>
+																											<td></td>
+																										</tr>
+
+																									</table>
+																									<p
+																										style="color: #cecece;line-height: 200%;font-size: 17pt;font-family: 'Nunito Sans', 'Nunito', sans-serif;">
+																										Em caso de problemas, entre em contato com a equipe de suporte,
+																										através do seguinte email:<br>
+																										<a style="color:#709dff;font-family:'Nunito','Nunito Sans',sans-serif;text-decoration: none !important;" href="mailto:suporte.cupcenter@gmail.com" target="_blank">suporte.cupcenter@gmail.com</a>
+																									</p>
+																									<table align="center" style="width: 100%">
+																										<p
+																											style="color: #cecece;line-height: 200%;font-size: 17pt;font-family: 'Nunito Sans', 'Nunito', sans-serif;margin-top: 3em; text-align: center; font-size: 12pt; bottom: 0;">
+																											&copy; Cup Center 2019</p>
+																									</table>
+
+																							</td>
+																						</tr>
+																					</tbody>
+																				</table>
+
+																			</td>
+																		</tr>
+																	</tbody>
+																</table>
+
+															</td>
+														</tr>
+													</tbody>
+												</table>
+
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+</td>
+</tr>
+
+</tbody>
+</table>
+`
+}
+
+export default Mail
