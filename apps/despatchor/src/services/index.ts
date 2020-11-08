31e92254c998f06183a814f45dd0b22778d701cf
Xubili <guivpw68@gmail.com>
2020-11-08 03:23:44 -0300

    added apollo graphql calls configuration

---
+++ apps/despatchor/src/services/index.ts
@@ -0,0 +1,33 @@
+import { split, ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import {createUploadLink} from 'apollo-upload-client'
+
+import baseUrl from '../app/constants/apiUrl'
+
+const wsLink = new WebSocketLink({
+	uri: `ws://${baseUrl.split('//')[1]}/graphql`,
+	options: {
+		reconnect: true
+	}
+})
+
+const uploadLink = createUploadLink({ uri: `${baseUrl}/graphql` })
+
+export const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query)
+		return (
+			definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+		)
+	},
+	wsLink,
+	uploadLink
+)
+
+const link = ApolloLink.from([splitLink])
+
+export const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache()
+})
