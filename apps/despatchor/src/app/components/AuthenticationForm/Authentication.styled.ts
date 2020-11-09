4f8fe4c8d28180512a26ff4acbb5e3e6e6a5c208
Xubili <guivpw68@gmail.com>
2020-11-08 23:09:28 -0300

    added Authentication div style & custom auth theme

---
+++ apps/despatchor/src/app/components/AuthenticationForm/Authentication.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import {Box} from '@material-ui/core'
+
+export const AuthenticationStyled = styled.div`
+  padding: 45px 24px;
+  display: flex;
+`
+
+export const BoxStyled = styled(Box)`
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
--- apps/despatchor/src/app/pages/Home/index.tsx
+++ apps/despatchor/src/app/pages/Home/index.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { AuthState } from 'apps/despatchor/src/store/reducers/auth/types'
 import AuthenticationForm from '../../components/AuthenticationForm'
 import AuthenticatedHome from '../../components/AuthenticatedHome'
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core'
 
 interface Props {
-	authUser: AuthState['authUser']
+  authUser: AuthState['authUser']
 }
 
+const theme = createMuiTheme()
+
 const Home: React.FC<Props> = ({authUser}) => {
-	return authUser ? <AuthenticatedHome authUser={authUser}/> : <AuthenticationForm />
+	return authUser ?
+		<AuthenticatedHome authUser={authUser} />
+	 :
+		<MuiThemeProvider theme={theme}>
+			<AuthenticationForm />
+		</MuiThemeProvider>
+
 }
 
 export default Home
