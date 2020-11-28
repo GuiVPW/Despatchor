d7dd7bbb1ffeb31309e930cee94f6287693a0741
Xubili <guivpw68@gmail.com>
2020-11-27 22:59:48 -0300

    changed apollo links configuration

--- apps/despatchor/src/services/index.ts
+++ apps/despatchor/src/services/index.ts
@@ -1,23 +1,29 @@
-import { split, ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client'
+import { split, ApolloClient, InMemoryCache, from } from '@apollo/client'
+import {setContext} from '@apollo/client/link/context'
 import { WebSocketLink } from '@apollo/client/link/ws'
 import { getMainDefinition } from '@apollo/client/utilities'
-import {createUploadLink} from 'apollo-upload-client'
+import { createUploadLink } from 'apollo-upload-client'
 
 import baseUrl from '../app/constants/apiUrl'
 
-const token = localStorage.getItem('Authorization')
+export const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('Authorization')
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	}
+})
 
 const wsLink = new WebSocketLink({
-	uri: `ws://${baseUrl.split('//')[1]}/graphql`,
+	uri: `${baseUrl.replace(/(https|http)/, 'ws')}/graphql`,
 	options: {
 		reconnect: true
 	}
 })
 
 const uploadLink = createUploadLink({
-	headers: {
-		'Authorization': token
-	},
 	uri: `${baseUrl}/graphql`
 })
 
@@ -32,7 +38,7 @@ export const splitLink = split(
 	uploadLink
 )
 
-const link = ApolloLink.from([splitLink])
+const link = from([splitLink, authLink])
 
 export const client = new ApolloClient({
 	link,
